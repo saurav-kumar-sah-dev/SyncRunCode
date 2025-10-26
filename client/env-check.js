72eafdf50e703879b78e95e9aa7de4e53fc71fbb
Sauravkumar903 <sauravshubham903@gmail.com>
2025-10-26 19:37:55 +0530

    Clean up project: Remove unnecessary files and executables

--- client/env-check.js
+++
@@ -1,5 +0,0 @@
-// Environment check script
-console.log('Environment Variables Check:');
-console.log('VITE_API_URL:', process.env.VITE_API_URL);
-console.log('VITE_SOCKET_URL:', process.env.VITE_SOCKET_URL);
-console.log('NODE_ENV:', process.env.NODE_ENV);
